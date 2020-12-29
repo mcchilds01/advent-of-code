4febfc287b8a8179d1d11ddb69751f597d9b7f33
Matthew Childs <76403824+mcchilds01@users.noreply.github.com>
2020-12-29 10:54:53 -0500

    Update day_2.py

--- script/day_2.py
+++ script/day_2.py
@@ -1,15 +1,17 @@
+import re
+
 with open("puzzle2_input.txt", "r") as x:
-    d = []
-    dPlus = []
     Count = 1000
-    for line in x:
-        d.append(line.split(": "))
-    for i in d:
-        dPlus.append(i[0].split("-"))
-    for i in range(len(d)):
-        if dPlus[i][-1][-1] not in d[i][1]:
+    keysAndPwds = [re.split(':| ', line) for line in x]
+    keyRange = [i[0].split("-") for i in keysAndPwds]
+    keys = [i[1] for i in keysAndPwds]
+    pswds = [i[3] for i in keysAndPwds]
+    for i in range(len(keysAndPwds)):
+        if keys[i] not in pswds[i]:
+            print(keys[i])
             Count -= 1
-        elif d[i][1].count(dPlus[i][-1][-1]) not in range(int(dPlus[i][0]), int(dPlus[i][1][:-2])+1):
+            print(keys[i], pswds[i])
+        elif pswds[i].count(keys[i]) not in range(int(keyRange[i][0]), (int(keyRange[i][1])+1)): 
             Count -= 1
     print(Count)
 
